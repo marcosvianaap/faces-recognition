1c62038bd4e86d3bc9e910d138d97b3e5a707502
marcos <marcosap103@gmail.com>
2023-05-27 02:15:25 -0300

    update venv

--- eigenfaces.py
+++ eigenfaces.py
@@ -1,51 +1,59 @@
+
 import cv2
 import numpy as np
+import face_recognition
+import os
 
-# Carrega o classificador pré-treinado para detecção de faces
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+ 
+path = 'training-pictures'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+ 
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
 
-# Carrega as imagens de treinamento positivas (imagens de faces)
-positive_images = []
-for i in range(1, 11):
-    img = cv2.imread(f'positives/face_{i}.jpg')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    positive_images.append(gray)
+ 
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+ 
+cap = cv2.VideoCapture(0)
+ 
+while True:
+    success, img = cap.read()
 
-# Extrai as features das imagens de treinamento positivas
-positive_features = np.array([cv2.resize(img, (100, 100)).flatten() for img in positive_images])
-
-# Carrega as imagens de treinamento negativas (imagens sem faces)
-negative_images = []
-for i in range(1, 11):
-    img = cv2.imread(f'negatives/negative_{i}.jpg')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    negative_images.append(gray)
-
-# Extrai as features das imagens de treinamento negativas
-negative_features = np.array([cv2.resize(img, (100, 100)).flatten() for img in negative_images])
-
-# Junta as features positivas e negativas em um único conjunto de treinamento
-X_train = np.concatenate((positive_features, negative_features), axis=0)
-
-# Cria os rótulos para as imagens de treinamento positivas (1) e negativas (0)
-y_train = np.array([1] * positive_features.shape[0] + [0] * negative_features.shape[0])
-
-# Treina um modelo baseado em autovetores (Eigenfaces) usando o PCA
-pca = cv2.PCA_create()
-pca.fit(X_train)
-
-# Carrega a imagem de teste
-image = cv2.imread('test_image.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Aplica o modelo baseado em autovetores para a detecção de faces na imagem de teste
-faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-# Desenha retângulos ao redor das faces detectadas na imagem
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-# Exibe a imagem com as faces detectadas
-cv2.imshow('Faces', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+ 
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+ 
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+       
+        matchIndex = np.argmin(faceDis)
+ 
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+          
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+        
+ 
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
